68f21480a694820294e443c6c6b9fddc684e6894
aadityanaik007 <75658706+aadityanaik007@users.noreply.github.com>
2022-02-25 16:48:39 +0530

    Logic correction pending

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ socket.onopen = () => {
   const msg = {
     "method": "auth",
     "params": {
-      UserId: 2,
-      Token: 'mpouhfBwhqP3aRhsyZgg',
+      UserId: 3,
+      Token: 'I0kaBd4JNGZIcbp8Zgiq',
     }
   }
 
@@ -23,98 +23,93 @@ socket.onopen = () => {
   if (socket.readyState === 1) {
     socket.send(JSON.stringify(msg))
     socket.send(JSON.stringify(msg2))
-    socket.send(JSON.stringify({"method":"getActiveSymbols"}))   
-    socket.send(JSON.stringify({"method": "getAllData" }))
+    socket.send(JSON.stringify({ "method": "getActiveSymbols" }))
+    socket.send(JSON.stringify({ "method": "getAllData" }))
   }
 };
 
 const App = () => {
   const [Data, setData] = useState([]);
   var user_list = []
+  var userData_list = []
   var userData = {}
 
   useEffect(() => {
     socket.onmessage = (data) => {
       data = JSON.parse(data.data).NetPosition
       console.log(data);
-      for(let i=0;i<data.length;i++){
-        if(data[i]){
-          if(user_list.length === 0){
-            if(data['exchange'] ==="NSEFO"){
-              userData['nseMtm']+=data['netlivemtmt']
+      if(data){
+        console.log(user_list);
+
+        for (let i = 0; i < data.length; i++) {
+          if (user_list.length === 0) {
+            console.log('in');
+            if (data['exchange'] === "NSEFO") {
+              userData['userid'] = data[i]['userid']
+              userData['nseMtm'] += data[i]['netlivemtmt']
+              console.log('nseMtm',userData)
             }
-            else{
-              userData['sgxMtm']+=data['netlivemtmt']
+          else {
+            userData['userid'] = data[i]['userid']
+            userData['sgxMtm'] += data[i]['netlivemtmt']
+            console.log('sgxMtm===',userData)
+
+          }
+          userData_list.push(userData);
+          user_list.push(userData['userid']);
+          userData = {}
+          // console.log(userData_list)
+        }
+
+
+        else {
+          if (user_list.includes(data[i]['userid'])) {
+            for (let j = 0; j < userData_list.length; j++) {
+              if (userData_list[j]['userid'] === data[i]['userid']) {
+                if (data[i]['exchange'] === "NSEFO") {
+                  userData_list[j]['nseMtm'] += data[i]['netlivemtmt']
+                }
+                else {
+                  userData_list[j]['sgxMtm'] += data[i]['netlivemtmt']
+                }
+              }
             }
+          }
+
+          else {
+            userData = {}
+            user_list.push(data['userid']);
+            if (data['exchange'] === "NSEFO") {
+              userData['userid'] = data[i]['userid']
+              userData['nseMtm'] = data[i]['netlivemtmt']
+            }
+            else {
+              userData['userid'] = data[i]['userid']
+              userData['sgxMtm'] = data[i]['netlivemtmt']
+            }
+            userData_list.push(userData);
           }
         }
       }
-      let dt = data.map((item)=>{
-        if(item['ctclid'])
-        return {'userid':item['userid'],'ctclid':item['ctclid'],'buybrok':item['buybrok'],'netqty':item['netqty']}
-      })
-      // console.log();
-      setData(dt);
-      // setData(data);
-    };
-  }, []);
-  
-  // console.log(Data);
-  // console.log(Data);
-const data= Data
-  // const data = [
-  //   {
-  //     name: "Page A",
-  //     uv: '400',
-  //     pv: Math.random()*10000,
-  //     amt: 8000
-  //   },
-  //   {
-  //     name: "Page B",
-  //     uv: 3000,
-  //     pv: 1398,
-  //     amt: 2210
-  //   },
-  //   {
-  //     name: "Page C",
-  //     uv: 2000,
-  //     pv: 9800,
-  //     amt: 2290
-  //   },
-  //   {
-  //     name: "Page D",
-  //     uv: 2780,
-  //     pv: 3908,
-  //     amt: 2000
-  //   },
-  //   {
-  //     name: "Page E",
-  //     uv: 1890,
-  //     pv: 4800,
-  //     amt: 2181
-  //   },
-  //   {
-  //     name: "Page F",
-  //     uv: 2390,
-  //     pv: 3800,
-  //     amt: 2500
-  //   },
-  //   {
-  //     name: "Page G",
-  //     uv: 3490,
-  //     pv: 4300,
-  //     mv: 4511,
-  //     amt: 2100
-  //   }
-  // ];
+    }
+      // console.log(userData_list);
+      
+    }
+    // setData(dt);
+    // setData(data);
+}, []);
+
+// console.log(Data);
+// console.log(Data);
 
 
-  return (
-    <div>
-      {/* Data Received:==== {data} */}
-      <Example data={data} />
-    </div>
-  )
+return (
+  <div>
+    hi
+    {/* Data Received:==== {data} */}
+    {/* <Example data={data} /> */}
+  </div>
+)
 }
 
 export default App; 
